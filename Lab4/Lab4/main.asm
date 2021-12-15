a2f9e9d8323b26a3de52985a8a45334b2a3e0f6e
Hadi Ansari <hadan326@student.liu.se>
2021-12-15 16:00:57 +0100

    Lab4 är klar för inlämning

--- Lab4/Lab4/main.asm
+++ Lab4/Lab4/main.asm
@@ -1,8 +1,6 @@
 ;
 ; lab4.asm
-;
 ; Created: 14/12/2021 12:34:27
-; Author : hadia
 ;
 	.dseg
 	.org	$200
@@ -48,8 +46,9 @@ MAIN:
 	call	LINE_INIT
 
 FOREVER:
-	;call	LINE_PRINT
-	
+	call	LINE_PRINT
+	lds		r16, CUR_POS
+	call	LCD_COL
 	call	KEY_READ
 	call	HANDLE_KEY
 	jmp		FOREVER
@@ -63,16 +62,9 @@ PORT_INIT:
 
 	ret
 
-BACKLIGHT_ON:
-	sbi		PORTB, BLGT
-	ret
-
-BACKLIGHT_OFF:
-	cbi		PORTB, BLGT
-	ret
 
 LCD_INIT:
-	call	BACKLIGHT_ON
+	sbi		PORTB, BLGT	; Turn the backlight on
 	call	WAIT		; wait for LCD ready
 
 	; initiate 4-bit mode
@@ -98,22 +90,24 @@ LCD_INIT:
 	ldi		r16, E_MODE
 	call	LCD_COMMAND
 
-	; set CUR_POS to Zero
-	clr		r16
+	; initiate the CUR_POS
+	ldi		r16, 4
 	sts		CUR_POS, r16
 
 	ret
 
-LINE_INIT:
-	ldi		r16, 16 + 1
+LINE_INIT:		; Empty the LINE to have an enpty screen on start
+	ldi		r16, 16
 	ldi		YH, HIGH(LINE)
 	ldi		YL, LOW(LINE)
 
-SET_ZERO:
-	clr		r17
+FILL_BYTE:
+	ldi		r17, $20 ; ASCII for space
 	st		Y+, r17
 	dec		r16
-	brne	SET_ZERO
+	brne	FILL_BYTE
+	clr		r16
+	st		Y, r16
 	ret
 
 WAIT:
@@ -171,18 +165,7 @@ LCD_COMMAND:
 LCD_HOME:
 	ldi		r16, RET_HOME
 	call	LCD_COMMAND
-
 	ret
-
-LCD_ERASE:
-	ldi		r16, LCD_CLR
-	call	LCD_COMMAND
-
-	; set CUR_POS to Zero
-	clr		r16
-	sts		CUR_POS, r16
-	ret
-
 
 ADC_READ8:
 	; Configuration
@@ -222,6 +205,15 @@ NOT_AF:
 	call LCD_ASCII
 	pop r16
 	ret
+	; We calculate the fallowing results using LCD_PRINT_HEX
+	;
+	; RIGHT: 00, UP: 24, LEFT:102, DOWN: 63, SELECT:159 otherwise 255
+	;
+	; Button			None	SELECT		 LEFT		DOWN	  UP    	RIGHT
+	;----------------------------------------------------------------------------
+	; RETURN VALUE		0		  1			  2			  3		  4 		 5	
+	;	INTERVAL	255-208		207-131		130-83		82-44	43-12		12-0
+
 
 KEY:
 	call	ADC_READ8
@@ -310,30 +302,68 @@ HANDLE_KEY:
 	cpi		r16, DOWN
 	breq	DOWN_PRESSED
 
+	in		r16, PORTB
+	ldi		r17, $04
+	eor		r16, r17
+	out		PORTB, r16
+	
+	jmp		RETURN
 
 LEFT_PRESSED:
 	lds		r16, CUR_POS
+	cpi		r16, 0
+	breq	RETURN
 	dec		r16
 	call	LCD_COL
 	jmp		RETURN
 
 RIGHT_PRESSED:
 	lds		r16, CUR_POS
+	cpi		r16, 15
+	breq	RETURN
 	inc		r16
 	call	LCD_COL
 	jmp		RETURN
 UP_PRESSED:
-
+	call	LINE_LOOKUP
+	ld		r16, Y
+	cpi		r16, $5A
+	breq	SPACE
+	cpi		r16, $20
+	breq	WRAP_AROUND_A
+	inc		r16
+	jmp		UPDATE_CHAR
+SPACE:
+	ldi		r16, $20
+	jmp		UPDATE_CHAR
+WRAP_AROUND_A:
+	ldi		r16, $41
+	jmp		UPDATE_CHAR
 DOWN_PRESSED:
-
+	call	LINE_LOOKUP
+	ld		r16, Y
+	cpi		r16, $41
+	breq	SPACE
+	cpi		r16, $20
+	breq	WRAP_AROUND_Z
+	dec		r16
+	jmp		UPDATE_CHAR
+WRAP_AROUND_Z:
+	ldi		r16, $5A
+	
+UPDATE_CHAR:
+	st		Y, r16
 
 RETURN:
 	ret
 
+LINE_LOOKUP:		; find the value stored in LINE based on CUR_POS and adjust the Y to point to that byte
+	ldi		YH, HIGH(LINE)
+	ldi		YL, LOW(LINE)
 
-	; RIGHT: 00, UP: 24, LEFT:102, DOWN: 63, SELECT:159 otherwise 255
-	;
-	; Button			None	SELECT		 LEFT		DOWN	  UP    	RIGHT
-	;----------------------------------------------------------------------------
-	; RETURN VALUE		0		  1			  2			  3		  4 		 5	
-	;	INTERVAL	255-208		207-131		130-83		82-44	43-12		12-0
+	lds		r17, CUR_POS
+	add		YL, r17
+	clr		r17
+	adc		YH, r17
+	
+	ret
